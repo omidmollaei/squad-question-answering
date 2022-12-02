6997ccc444d4cdf4093f1c06135666ab40b840ff
unknown <omidmollaei2002@gmail.com>
2022-12-02 10:03:30 +0330

    create Paramaters class to hold all required (hyper)parameters in one place.

--- dataset.py
+++ dataset.py
@@ -1,4 +1,12 @@
 """load and preprocess SQuAD dataset, build tokenizer and convert dataset into a tf-dataset"""
 
+import os
+import datasets
+import pandas as pd
+import tensorflow as tf
+from tqdm import tqdm
 
 
+class Parameters(object):
+    def __init__(self, **kwargs):
+        self.__dict__.update(**kwargs)
