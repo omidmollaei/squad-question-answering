ebda0d1ce0f05089f996293f065ad7588f86ca78
unknown <omidmollaei2002@gmail.com>
2022-12-21 16:59:25 +0330

    define padding functions for both encoder and decoder

--- model.py
+++ model.py
@@ -34,3 +34,16 @@ class PositionalEncoding(tf.keras.layers.Layer):
     def call(self, inputs: tf.Tensor):
         return inputs + self.pos_encoding[:, : tf.shape(inputs)[1], :]
 
+
+def create_padding_mask(x: tf.Tensor):
+    mask = tf.cast(tf.math.equal(x, 0), dtype=tf.float32)
+    return mask[:, tf.newaxis, tf.newaxis, :]
+
+
+def create_look_ahead_mask(x: tf.Tensor):
+    seq_len = tf.shape(x)[1]
+    look_ahead_mask = 1 - tf.linalg.band_part(
+        tf.ones((seq_len, seq_len), dtype=tf.float32), -1, 0
+    )
+    padding_mask = create_padding_mask(x)
+    return tf.maximum(look_ahead_mask, padding_mask)
